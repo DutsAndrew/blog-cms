ad8f907ec8ed51fed9448b325f67893cd3588dd2
Andrew Dutson <Dutsandrew@gmail.com>
2023-03-27 21:11:32 -0600

    routes set up for card page changing

---
+++ app/account/page.tsx
@@ -0,0 +1,5 @@
+export default function AccountManagement() {
+  return (
+    <p>Account Management</p>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,43 @@
+'use client';
+
 import styles from './page.module.css';
 import uniqid from 'uniqid';
+import Link from 'next/link';
 
 export default function Home() {
 
-  const crudOperations = ["Create Post", "Update Post", "Delete Post", "Account Management"];
+  const crudOperations = [
+    {
+      title: "Create Post",
+      route: "/post/create",
+    },
+    {
+      title: "Update Post",
+      route: "/post/update"
+    },
+    {
+      title: "Delete Post",
+      route: "/post/delete",
+    },
+    {
+      title: "Account Management",
+      route: "/account"
+    },
+  ];
 
   return (
     <main className={styles.cmsCardsContainer}>
       {crudOperations.map((operation) => {
-        return <div className={styles.operationContainer} key={uniqid()}>
-          <p className={styles.operationText}>
-            {operation}
-          </p>
-        </div>
+        return <Link href={`${operation.route}`}>
+          <div 
+            className={styles.operationContainer}
+            key={uniqid()}
+          >
+            <p className={styles.operationText}>
+              {operation.title}
+            </p>
+          </div>
+        </Link>
       })}
     </main>
   )
