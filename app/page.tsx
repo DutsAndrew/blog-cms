180127d8f3d518f97285a2d2ea8a06c7e42ab326
Andrew Dutson <Dutsandrew@gmail.com>
2023-03-27 20:47:11 -0600

    cms cards render on load

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import styles from './page.module.css'
+import styles from './page.module.css';
+import uniqid from 'uniqid';
 
 export default function Home() {
+
+  const crudOperations = ["Create Post", "Update Post", "Delete Post", "Account Management"];
+
   return (
-    <main className={styles.main}>
-      <p>Hello</p>
+    <main className={styles.cmsCardsContainer}>
+      {crudOperations.map((operation) => {
+        return <div className={styles.operationContainer} key={uniqid()}>
+          <p className={styles.operationText}>
+            {operation}
+          </p>
+        </div>
+      })}
     </main>
   )
 }
