fd3c88f2f282dc315a45163821b735e3e524b454
DutsAndrew <dutsandrew@gmail.com>
2023-06-19 14:42:58 -0600

    anvil svg loads from public folder

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import anvilSVG from '../../public/anvil.svg';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
@@ -68,7 +67,7 @@ const Header = () => {
         <div className="header-logo-container">
           <img 
             className="anvil-svg"
-            src={anvilSVG}
+            src="/anvil.svg"
             >
           </img>
           <h1>
